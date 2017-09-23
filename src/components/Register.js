cea404c24ae23962de610ce999f02b2e25959013
pansitwattana <pansit.wt@gmail.com>
2017-09-23 20:07:37 +0700

    connect firebase

--- src/components/Register.js
+++ src/components/Register.js
@@ -1,5 +1,7 @@
-import React from 'react'
+import React, { Component } from 'react'
 import styled from 'styled-components'
+import firebase from 'firebase'
+import { withRouter } from 'react-router-dom'
 import { Input, Button } from 'semantic-ui-react'
 import Header from './commons/Header'
 import Logo from './commons/Logo'
@@ -11,18 +13,35 @@ const Form = styled.div`
   flex-direction: column;
 `
 
-const Register = () => (
-  <div>
-    <Logo />
-    <Header text="Register" />
-    <Form>
-      <Input icon="user" iconPosition="left" placeholder="Username" />
-      <Input icon="mail" iconPosition="left" placeholder="abc@gmail.com" />
-      <Input icon="lock" type="password" iconPosition="left" placeholder="Password" />
-      <Input icon="lock" type="password" iconPosition="left" placeholder="Confirm Password" />
-      <Button positive>Login</Button>
-    </Form>
-  </div>
-)
+class Register extends Component {
+  state = {
+    username: '',
+    mail: '',
+    password: '',
+    comfirmPassword: '',
+  }
 
-export default Register
+  onSubmit = () => {
+    firebase.auth().createUserWithEmailAndPassword(this.state.mail, this.state.password)
+      .then(response => this.props.history.push('/'))
+      .catch(error => alert(error.message))
+  }
+
+  render() {
+    return (
+      <div>
+        <Logo />
+        <Header text="Register" />
+        <Form>
+          <Input icon="user" iconPosition="left" placeholder="Username" onChange={(event, username) => this.setState({ username: username.value })} />
+          <Input icon="mail" iconPosition="left" placeholder="abc@gmail.com" onChange={(event, mail) => this.setState({ mail: mail.value })} />
+          <Input icon="lock" type="password" iconPosition="left" placeholder="Password" onChange={(event, password) => this.setState({ password: password.value })} />
+          <Input icon="lock" type="password" iconPosition="left" placeholder="Confirm Password" onChange={(event, password) => this.setState({ comfirmPassword: password.value })} />
+          <Button positive onClick={this.onSubmit}>Register</Button>
+        </Form>
+      </div>
+    )
+  }
+}
+
+export default withRouter(Register)
--- src/index.js
+++ src/index.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import firebase from 'firebase'
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+
+const config = {
+  apiKey: 'AIzaSyAtau_ldYK1E4al5ljsTxtVw4Gfdnajk_I',
+  authDomain: 'ezam-51dcc.firebaseapp.com',
+  databaseURL: 'https://ezam-51dcc.firebaseio.com',
+  projectId: 'ezam-51dcc',
+  storageBucket: 'ezam-51dcc.appspot.com',
+  messagingSenderId: '448662049456',
+};
+firebase.initializeApp(config)
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
