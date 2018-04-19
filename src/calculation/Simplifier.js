6266adb6a2a7aab0f613c9a74f156e3ace2a4991
pansitwattana <pansit.wt@gmail.com>
2018-04-19 22:58:44 +0700

    fix simplify

--- src/calculation/Simplifier.js
+++ src/calculation/Simplifier.js
@@ -1,5 +1,6 @@
 import Simplify from './Simplify'
 import Split from './SplitEquation'
+import SubstituteLatex from './SubstituteLatex'
 
 export default (expression) => {
   const equation = Split(expression)
@@ -8,11 +9,16 @@ export default (expression) => {
     return expression
   }
 
+  let result = ''
   if (equation.rhs && equation.lhs) {
     const lhs = Simplify(equation.lhs) || equation.lhs
     const rhs = Simplify(equation.rhs) || equation.rhs
-    return lhs + ' = ' + rhs
+    result = lhs + ' = ' + rhs
+  } else {
+    result = Simplify(expression)
   }
   
-  return Simplify(expression)
+  const resultSub = SubstituteLatex(result)
+
+  return resultSub
 }
---
+++ src/calculation/SubstituteLatex.js
@@ -0,0 +1,37 @@
+import math from 'mathjs'
+
+const regex = /\\frac({?[0-9]*})({?[0-9]*})/g
+const replaceRegex = /(}|{)/g
+
+export default (latex) => {
+  let m;
+  let matches = []
+  while ((m = regex.exec(latex)) !== null) {
+    // This is necessary to avoid infinite loops with zero-width matches
+    if (m.index === regex.lastIndex) {
+        regex.lastIndex++;
+    }
+    // The result can be accessed through the `m`-variable.
+    let firstValue = 1
+    let replaceString = ''
+    m.forEach((match, groupIndex) => {
+      if (groupIndex == 0) {
+        replaceString = match
+      }
+      else if (groupIndex == 1) {
+        firstValue = match.replace(replaceRegex, '')
+      } else if (groupIndex == 2) {
+        const secondValue = match.replace(replaceRegex, '')
+        matches.push({ value: firstValue / secondValue, replaceString })
+      }
+    });
+  }
+
+  // const result = latex.replace(regex, value)
+  let result = latex
+  matches.forEach(({ value, replaceString }) => {
+    result = result.replace(replaceString, math.format(value, { precision: 5 }))
+  })
+
+  return result
+}
