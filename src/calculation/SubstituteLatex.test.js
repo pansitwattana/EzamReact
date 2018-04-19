9dc6e5269a57086decfd720a86f3d5ba237af9f4
pansitwattana <pansit.wt@gmail.com>
2018-04-19 22:58:11 +0700

    fix simplify

---
+++ src/calculation/SubstituteLatex.test.js
@@ -0,0 +1,16 @@
+import SubstituteLatex from './SubstituteLatex'
+
+test('first case', () => {
+  const latex = '4x^2+\\frac{4}{3}=0'
+  expect(SubstituteLatex(latex)).toBe('4x^2+1.3333=0')
+})
+
+test('second case', () => {
+  const latex = '\\frac{50}{100}+\\frac{7}{5}=0'
+  expect(SubstituteLatex(latex)).toBe('0.5+1.4=0')
+})
+
+test('third case', () => {
+  const latex = '\\frac{\\frac{7}{5}}{100}=0'
+  expect(SubstituteLatex(latex)).toBe('\\frac{1.4}{100}=0')
+})
